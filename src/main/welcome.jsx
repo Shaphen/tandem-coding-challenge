d6a0fe7eee2e2aba4fed1614e85a37124804d72b
Shaphen <swpburn@gmail.com>
2020-11-01 10:59:12 -0800

    Add basic styling to welcome page

--- src/main/welcome.jsx
+++ src/main/welcome.jsx
@@ -7,12 +7,14 @@ class Welcome extends React.Component {
   render() {
     return(
       <div className="welcome-container">
-        <p id="welcome-title">Welcome to Trividem!</p>
-        <Link to="/trivia-game">Start Trivia</Link>
+        <div id="welcome-box">
+          <p id="welcome-title">Welcome to Trividem!</p>
+          <Link to="/trivia-game" id="start-game">START TRIVIA</Link>
+        </div>
       </div>
     )
   }
-  
+
 }
 
 export default Welcome;
